837d4f4f0aa4473c67c77ce7a8f51629535a7a5b
Valdryan Ivandito <developeryayasanopenheaven.com>
2024-10-31 09:58:39 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+const year = 2024;
+const message = "Hello World!";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Cardano Workshop Maranatha {year}</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
